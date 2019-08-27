e56a7af7efb3bb9c26ed95f42bfe131753cde060
jaeyp <jp.inseoul@gmail.com>
2019-08-27 10:59:30 -0400

    removed comments

--- src/react/App.js
+++ src/react/App.js
@@ -15,17 +15,7 @@ import '../assets/calculator.scss'
 import { Provider } from 'react-redux';
 import store from './store';
 import Calculator from './component/calculator';
-// electron
-//import { channels } from '../shared/constants';
-//const { ipcRenderer } = window;
-/**
- *  To disable Electron Mode (enable React Mode)
- *  1. sets ELECTRON_MODE = false
- *  2. comments 'BROWSER=none' in .env file
- */
-//const ELECTRON_MODE = false;
 
-// electron
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -33,17 +23,8 @@ class App extends React.Component {
       appName: 'Calculator',
       appVersion: '0.3.0',
     };
-/*	  
-    if(ELECTRON_MODE) {
-      ipcRenderer.send(channels.APP_INFO);
-      ipcRenderer.on(channels.APP_INFO, (event, arg) => {
-        ipcRenderer.removeAllListeners(channels.APP_INFO);
-        const { appName, appVersion } = arg;
-        this.setState({ appName, appVersion });
-      });
-    }
-*/
   }
+  
   render() {
     const { appName, appVersion } = this.state;
     return (
